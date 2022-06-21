027e5f4676dcc6aebe1cacfacaea8547c77a8aab
temple <utemple169@gmail.com>
2022-06-21 02:26:26 +0100

    updated calculator

--- script.js
+++ script.js
@@ -7,24 +7,26 @@ let operations={
 function calculator(){
     let operand1= prompt("Enter First Number")
     let operation = prompt("What Operations Would You Like To Perform [+,-,/,*]")
-let operand2= prompt("Enter Second Number")
+    let operand2= prompt("Enter Second Number")
     let result=0
     if (isNaN(operand1) || isNaN(operand2)) {
         throw   new Error("Alphabets  Not allowed Only Number")
     }
     
     else{
+        parseFloat(operand1)
+        parseFloat(operand2)
         if(operation ==="+"){
-            result+=parseFloat(operand1)+parseFloat(operand2)
+            result+=operand1+operand2
         }
         else if(operation ==="-"){
-            result=parseFloat(operand1)-parseFloat(operand2)
+            result=operand1-operand2
         }
         else if(operation ==="/"){
             result= operand1 /operand2
         }
         else if(operation ==="*"){
-            result=parseFloat(operand1)*parseFloat(operand2)
+            result=operand1*operand2
         }
         else{
             alert("invalid Operation Cannot Perform operation")
